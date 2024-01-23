188d1aac98c774fdf844c087b39344bff3941682
elmarzouguidev <elmarzouguiabdelghafour@gmail.com>
2024-01-23 11:37:17 +0100

    add Offre Model

---
+++ app/Models/WEDOAPP/Offre.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Models\WEDOAPP;
+
+use App\Traits\GetModelByKeyName;
+use App\Traits\HasSlug;
+use App\Traits\UuidGenerator;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Offre extends Model
+{
+    use HasFactory;
+    use UuidGenerator;
+    use GetModelByKeyName;
+    use HasSlug;
+    
+
+    /**
+     * @var string[]|array<int,string>
+     */
+    protected $fillable = [
+        'uuid',
+        
+        
+        'is_active',
+    ];
+
+    /**
+     * @var string[]|array<int,string>
+     */
+    protected $casts = [
+        'is_active' => 'boolean',
+    ];
+
+    // Relationships
+
+    // Helper Methods
+}
--- database/migrations/2024_01_23_101250_create_offres_table.php
+++ database/migrations/2024_01_23_101250_create_offres_table.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Enums\DTO\ServiceStatus;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -12,15 +11,15 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('services', function (Blueprint $table) {
+        Schema::create('offres', function (Blueprint $table) {
             $table->id();
             $table->uuid()->nullable();
-            $table->string('name', 200);
+            $table->string('title');
             $table->string('slug');
             $table->mediumText('content')->nullable();
             $table->longText('body')->nullable();
-
             $table->string('image')->nullable();
+
             $table->boolean('is_active')->default(true);
             $table->timestamps();
         });
@@ -31,6 +30,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('services');
+        Schema::dropIfExists('offres');
     }
 };

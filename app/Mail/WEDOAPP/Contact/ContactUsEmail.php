85aad5301386f754de614467d6755d662be23bf1
elmarzouguidev <elmarzouguiabdelghafour@gmail.com>
2024-02-12 15:24:19 +0100

    make contact form dynamique with validations v-4

--- app/Mail/WEDOAPP/Contact/ContactUsEmail.php
+++ app/Mail/WEDOAPP/Contact/ContactUsEmail.php
@@ -10,7 +10,7 @@ use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
-class ContactUsEmail extends Mailable implements ShouldQueue
+class ContactUsEmail extends Mailable
 {
     use Queueable, SerializesModels;
 

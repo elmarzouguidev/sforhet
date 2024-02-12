45d625d26f0168e8566baab28376d8cabbcc8d33
elmarzouguidev <elmarzouguiabdelghafour@gmail.com>
2024-02-12 12:46:47 +0100

    force update logo with colors

--- resources/views/layouts/__parts/css_files.blade.php
+++ resources/views/layouts/__parts/css_files.blade.php
@@ -4,4 +4,4 @@
 <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
 <link rel="stylesheet" href="{{ asset('assets/css/swiper-bundle.min.css') }}" />
 <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}?ver={{ rand(10, 850) }}">
-<link rel="stylesheet" href="{{ asset('assets/css/responsive.css') }}">
+<link rel="stylesheet" href="{{ asset('assets/css/responsive.css') }}?ver={{ rand(10, 850) }}">

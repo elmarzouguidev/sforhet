8c4193aeb58024039d1c74907d1adef6d6dc71b9
elmarzouguidev <elmarzouguiabdelghafour@gmail.com>
2024-02-12 15:26:14 +0100

    make contact form dynamique with validations v-5

--- resources/views/emails/contact/contactus.blade.php
+++ resources/views/emails/contact/contactus.blade.php
@@ -1,6 +1,5 @@
 <p>Nom Complet : {{ $data['full_name'] }}</p>
-<p>Country : {{ $data['country'] }}</p>
-<p>email : {{ $data['email'] }}</p>
-<p>tele : {{ $data['phone'] }}</p>
+<p>E-mail : {{ $data['email'] }}</p>
+<p>Téléphone : {{ $data['phone'] }}</p>
 <br>
 <p>Message : {{ $data['message'] }}</p>

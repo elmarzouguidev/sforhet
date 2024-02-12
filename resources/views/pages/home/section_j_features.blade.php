f2d329cc84b8b5b2561dbd711b0392fbaec7261a
elmarzouguidev <elmarzouguiabdelghafour@gmail.com>
2024-02-12 16:22:03 +0100

    make contact form dynamique with validations v-11

--- resources/views/pages/home/section_j_features.blade.php
+++ resources/views/pages/home/section_j_features.blade.php
@@ -4,19 +4,19 @@
             <div class="feature-left d-flex">
                 <div class="feature-img-card">
                     <h3>Espace <br> candidats</h3>
-                    <img src="{{ asset('assets/imgs/feature-img-1.png') }}" alt="Feature" />
+                    <img src="{{ Voyager::image(setting('home.candidat_image')) }}" alt="Espace candidats" />
                 </div>
                 <div class="feature-img-card">
                     <h3>Espace <br> entreprise</h3>
-                    <img src="{{ asset('assets/imgs/feature-img-2.png') }}" alt="Feature" />
+                    <img src="{{ Voyager::image(setting('home.entreprise_image')) }}" alt="Espace entreprise" />
                 </div>
             </div>
 
             <div class="feature-right">
                 <div class="feature-content">
                     <h5 class="section-subtitle">NOS ESPACES</h5>
-                    <h1 class="section-title">Seamless productivity with our new app</h1>
-                    <p>Download Slack and experience a new era of organization and accomplishment.</p>
+                    <h1 class="section-title" style="color:#F6950C !important">Seamless productivity with our new app
+                    </h1>
                     {{-- <ul>
                         <li>
                             <i class="las la-check"></i> PPD Development

61ddff6ccccb22e57bc5b519985cfdd617b19f3a
elmarzouguidev <elmarzouguiabdelghafour@gmail.com>
2024-02-12 18:21:56 +0100

    update some stuffs v-2

--- resources/views/pages/offres/single/section_b_details.blade.php
+++ resources/views/pages/offres/single/section_b_details.blade.php
@@ -1,24 +1,25 @@
-<section class="event-details-area">
+<section class="about-service3-area about-service5-area">
     <div class="custom-container">
         <div class="custom-row align-items-center">
             <div class="img-box">
                 <img src="{{ Voyager::image($offre->image) }}" alt="{{ $offre->title }}" />
             </div>
-            <div class="content-box">
 
+            <div class="content-box">
+                {{-- <h1>Why our services are <br>
+                    better than others?</h1> --}}
                 <p>{!! $offre->body !!}</p>
 
-                <ul>
-                    <li>
-                        <i class="las la-map-marker"></i> {{ $offre->offre_location }}
-                    </li>
-                    <li>
-                        <i class="las la-calendar"></i> {{ $offre->offre_date?->format('d/m/Y') }}
-                    </li>
-                </ul>
-
-                <a href="#applyform" class="theme-btn">Postuler</a>
             </div>
+            <ul>
+                <li>
+                    <i class="las la-map-marker"></i> {{ $offre->offre_location }}
+                </li>
+                <li>
+                    <i class="las la-calendar"></i> {{ $offre->offre_date?->format('d/m/Y') }}
+                </li>
+            </ul>
+            <a href="#applyform" class="theme-btn">Postuler</a>
         </div>
     </div>
 </section>
